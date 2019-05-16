cc603d9ff02fc286591b4299ac002d4e2a1b15c5
smoroso <stephane.moroso@gmail.com>
2019-05-16 15:37:06 +0200

    Ongoing

--- src/client/app/pages/concrete_form/containers/Form.js
+++ src/client/app/pages/concrete_form/containers/Form.js
@@ -2,81 +2,55 @@
 
 import React from "react";
 import PropTypes from "prop-types";
+import NestedForm from "common/containers/NestedForm";
+import {fetchFormDefinition} from "common/services/fetcherService";
 
-const FormGenerator = ({ sections = [] }) => (
-  <div>
-    {sections.map(s => (
-      <div key={`section_${s.name}`}>
-        <h3>{s.caption}</h3>
-        {/* <FormSection name={s.name} component="div">
-          <Row>
-            {s.fields.map(f => (
-              <Col md={6} key={f.name}>
-                <Field
-                  name={f.name}
-                  component={stringToComponentMapper[f.component]}
-                  label={f.label}
-                />
-              </Col>
-            ))}
-          </Row>
-        </FormSection> */}
-      </div>
-    ))}
-  </div>
-);
-FormGenerator.propTypes = {
-  sections: PropTypes.array
-};
-
-export default class Form extends React.Component {
+class Form extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      formDef: {}
+      steps: []
     };
+
+    this.resetForm = this.resetForm.bind(this);
+    this.submitForms = this.submitForm.bind(this);
   }
 
   componentDidMount() {
     const {match} = this.props;
-    return this.fetchFormDefinition(match.params.id);
+    return this.resetForm(match.params.id);
   }
 
-  fetchFormDefinition(formId) {
-    return fetch(`/api/getFormDefinition/${formId}`)
-      .then(res => res.json())
-      .then(data => this.setState({ formDef: data.formDef }));
+  resetForm() {
+    this.setState({steps:[]});
+    
+    return fetchFormDefinition("nested")
+      .then(this.setState.bind(this));
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    // eslint-disable-next-line no-console
+    console.log(this.state);
   }
 
   render() {
-    const { match } = this.props;
-    const { formDef } = this.state;
+    const { steps } = this.state;
     return (
       <div>
-        <h3>Form for: {match.params.id}</h3>
-        <div>{formDef && 
-          <form>
-            <button
-              onClick={this.handleFormSubmit}
-              className="btn btn-primary">
-              Save
-            </button>
-            &nbsp;
-            <button
-              onClick={this.handleCancel}
-              className="btn btn-default">
-              Cancel
-            </button>
-          </form>
-        }</div>
+        {
+          steps.length && 
+          <NestedForm steps={steps} handleResetClick={this.resetForm} handleSubmitClick={this.submitForm}/>
+        }
       </div>
     );
   }
 }
 
 Form.propTypes = {
-  match: PropTypes.object
+  match: PropTypes.object,
+  steps: PropTypes.array
 };
 
-
+export default Form;
