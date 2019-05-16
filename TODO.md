edad4cecdd0832eddc7b21d747587caeaa60ed34
smoroso <stephane.moroso@gmail.com>
2019-05-16 13:08:18 +0200

    Chore: Clean up

--- TODO.md
+++ TODO.md
@@ -1,5 +1,2 @@
 # TODO LIST
-- dbounce and throttle the input change
-- make inputs validate themselves
-- make the form be valid only when all its inputs are valid
-- asynchronous validation. Maybe make it default thanks to debounce
+- asynchronous validation?
--- src/client/app/pages/static_form/containers/List.js
+++ src/client/app/pages/static_form/containers/List.js
@@ -5,10 +5,6 @@ import PropTypes from "prop-types";
 import { Route, Link } from "react-router-dom";
 import BasicForm from "pages/static_form/containers/BasicForm";
 import ConnectedInputsForm from "pages/static_form/containers/ConnectedInputsForm";
-import NestedForm from "pages/static_form/containers/nested_form";
-import NestedNestedForm from "pages/static_form/containers/BasicForm";
-import SavePendingForm from "pages/static_form/containers/BasicForm";
-import CssCustomizationForm from "pages/static_form/containers/BasicForm";
 import navStyles from "common/containers/navigation.scss";
 
 function List({ match }) {
@@ -17,18 +13,10 @@ function List({ match }) {
       <ul className={navStyles.navbar}>
         <li><Link className={navStyles.button2} to={`${match.url}/basic`}>Basic</Link></li>
         <li><Link className={navStyles.button2} to={`${match.url}/connected_inputs`}>Connected Inputs</Link></li>
-        <li><Link className={navStyles.button2} to={`${match.url}/nested`}>Nested</Link></li>
-        <li><Link className={navStyles.button2} to={`${match.url}/nested_nested`}>Nested Nested</Link></li>
-        <li><Link className={navStyles.button2} to={`${match.url}/save_pending`}>Save Pending</Link></li>
-        <li><Link className={navStyles.button2} to={`${match.url}/css_customization`}>Css Customization</Link></li>
       </ul>
 
       <Route path={`${match.path}/basic`} component={BasicForm} />
       <Route path={`${match.path}/connected_inputs`} component={ConnectedInputsForm} />
-      <Route path={`${match.path}/nested`} component={NestedForm} />
-      <Route path={`${match.path}/nested_nested`} component={NestedNestedForm} />
-      <Route path={`${match.path}/save_pending`} component={SavePendingForm} />
-      <Route path={`${match.path}/css_customization`} component={CssCustomizationForm} />
     </div>
   );
 }
