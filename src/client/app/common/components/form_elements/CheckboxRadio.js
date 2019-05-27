8698c4b6de438619c5ef198b0e00448fa0ec549c
Stephane Moroso <stephane.moroso@gmail.com>
2019-05-27 17:31:28 +0200

    feat: improves checkboxRadio

--- src/client/app/common/components/form_elements/CheckboxRadio.js
+++ src/client/app/common/components/form_elements/CheckboxRadio.js
@@ -1,26 +1,67 @@
 "use strict";
 
+/* eslint-disable react/prop-types */
 import React from "react";
 import PropTypes from "prop-types";
 import styles from "common/components/form_elements/inputs.scss";
 
-const CheckboxRadio = props => (
+/**
+ * @param {string} propsValue
+ * @param {string} inputValue
+ */
+const checkedRadioConditionFn = (propsValue, inputValue) => {
+  return propsValue === inputValue;
+};
+
+/**
+ * @param {array} propsValue
+ * @param {string} inputValue
+ */
+const checkedCheckboxConditionFn = (propsValue, inputValue) => {
+  return propsValue.includes(inputValue);
+};
+
+/** @returns {Object<string, function>} */
+const checkedConditionObj = {
+  radio: checkedRadioConditionFn,
+  checkbox: checkedCheckboxConditionFn
+};
+
+/**
+ * @param {Object} props
+ * @param {Object} valueObj
+ */
+const mapFn = (props, valueObj) => {
+  // @ts-ignore
+  const conditionFn = checkedConditionObj[props.type];
+  return (
+    <div key={valueObj.value}>
+      <input
+        type={props.type}
+        name={props.name}
+        value={valueObj.value}
+        checked={conditionFn(props.value, valueObj.value)}
+        id={valueObj.value}
+        onChange={props.handleInputChange}
+      />
+      <label htmlFor={valueObj.value}>{valueObj.label}</label>
+    </div>
+  );
+};
+
+/** @param {Object} props */
+const CheckboxRadio = (props) => (
   <div className={styles.checkboxRadioWrapper}>
-    {props.values.map(v => (
-      <div key={v.value}>
-        <input type={props.type} name={props.name} value={v.value} id={v.value} onChange={props.handleInputChange} />
-        <label htmlFor={v.value}>{v.label}</label>
-      </div>
-    ))}
+    {props.values.map(mapFn.bind(null, props))}
   </div>
 );
 
 CheckboxRadio.propTypes = {
-  type: PropTypes.string.isRequired,
-  handleInputChange: PropTypes.func.isRequired,
-  label: PropTypes.string,
+  values: PropTypes.array.isRequired,
+  type: PropTypes.string,
   name: PropTypes.string,
-  values: PropTypes.array
+  value: PropTypes.string || PropTypes.array,
+  handleInputChange: PropTypes.function
 };
 
 export default CheckboxRadio;
